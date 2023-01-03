cb4986dba6b6e5bb2e1999b81c63807fdb4a7a5a
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-03 14:54:03 +0100

    js_console

---
+++ main.js
@@ -0,0 +1,26 @@
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write(b);
+
+console.log(document.URL);
+
+console.log("Hello World");
+
+console.log("Anton");
+
+let firstName = "Anton";
+console.log(firstName);
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+// Anton ist 28 Jahre alt, von Beruf Trainer und ist verheiratet: true
+console.log(firstName + " ist " + age + " Jahre alt, von Beruf " + job + " und ist verheiratet: " + married)
